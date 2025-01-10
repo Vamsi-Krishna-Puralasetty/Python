21091e37946b40b60502c630e8009ed89e54ca66
Vamsi-Krishna-Puralasetty <puralasettyv@gmail.com>
2025-01-10 23:21:40 +0530

    compare first and last with remain

--- Easy_level/6.py
+++ Easy_level/6.py
@@ -7,26 +7,50 @@
 # Output : true  
 # Explanation : The middle digits 4,7,1 are less than first digit 8 and last  digit 9 .  
 
-num=int(input("Enter the number:"))
-temp=num
-last=num%10
-count=0
-while temp>0:
-    temp=temp%10
-    count+=1
+#method-1
+# num=int(input("Enter the number:"))
+# temp=num
+# last=num%10
+# count=0
+# while temp>0:
+#     temp=temp%10
+#     count+=1
+#     temp//=10
+# first=num//10**(count-1)
+# digit=str(num)
+# mid=digit[1:((len(digit))-1)]
+# remain=int(mid)
+# answer=True
+# for i in mid:
+#     remlast=remain%10
+#     if remlast<=first and remlast<=last:
+#         answer=True
+#         # print(true)
+#     else:
+#         answer=False
+#     # remain//=10
+#     # print(false)
+# print(answer)
+
+#method-2
+num=int(input("Enter a number :"))     
+temp=num                                  
+last=num%10                                 
+while temp>=10:                             
     temp//=10
-first=num//10**(count-1)
-digit=str(num)
-mid=digit[1:((len(digit))-1)]
-remain=int(mid)
+first=temp                                  
+num=str(num)                                
+remain=num[1:-1]                            
+
 answer=True
-for i in mid:
-    remlast=remain%10
-    if remlast<=first and remlast<=last:
-        answer=True
-        # print(true)
-    else:
+for i in remain:                            
+    if int(i)>first or int(i)>last:                      
         answer=False
-    # remain//=10
-        # print(false)
+        break
+    elif int(i)<first and int(i)<last:
+        answer=True
+    else:
+        answer=True                 
 print(answer)
+
+
