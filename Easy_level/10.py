97a7e9e9aecc69dfe59030aee4240207a09640e9
Vamsi-Krishna-Puralasetty <puralasettyv@gmail.com>
2025-01-11 23:42:18 +0530

    convert upper to lower case and viceversa

---
+++ Easy_level/10.py
@@ -0,0 +1,12 @@
+# Write a program to convert all the upper case letters in the given string to  lower case letter and vice versa.  
+# Testcase1 : JohnWick  
+# Output : jOHNwICK  
+# Explanation : All the upper case letters changed to lower case and vise  versa.  
+# Testcase1 : Korean  
+# Output : kOREAN  
+# Explanation : All the upper case letters changed to lower case and vise  versa.  
+
+#method-1
+str=input("Enter a value : ")
+print(str.swapcase())
+
