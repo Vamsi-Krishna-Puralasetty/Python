7baa80de5d6ecbf9bcba7766f50a60c67f48166c
Vamsi-Krishna-Puralasetty <puralasettyv@gmail.com>
2025-01-16 16:30:09 +0530

    reverse an array method-2

--- Easy_level/Array6.py
+++ Easy_level/Array6.py
@@ -7,13 +7,13 @@
 # The array [1, 2, 3, 4, 5] is reversed to [5, 4, 3, 2, 1].
 
 #method-1:
-# n = int(input("enter the number of elements: "))
-# arr=[]
-# for i in range(0,n):
-#     element = int(input("enter the element: "))
-#     arr.append(element)
+n = int(input("enter the number of elements: "))
+arr=[]
+for i in range(0,n):
+    element = int(input("enter the element: "))
+    arr.append(element)
 
-# print(arr[::-1])
+print(arr[::-1])
 
 #method-2:
 n = int(input("enter the number of elements: "))
