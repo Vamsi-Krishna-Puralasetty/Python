b364873da661ac45fc1ded7fd4e845ddba7cd994
Vamsi-Krishna-Puralasetty <puralasettyv@gmail.com>
2025-01-16 23:25:21 +0530

    check digit contains true

---
+++ Easy_level/string7.py
@@ -0,0 +1,28 @@
+# Check if String Contains Only Digits
+# Question: Write a function to check if a string contains only numeric digits.
+# Testcase: "12345"
+# Output: true
+# Explanation: The string "12345" consists only of digits, so the result is true.
+
+#method-1
+str=input("Enter the string : ")
+num="0123456789"
+ans=True
+for i in str:
+    if i not in num:
+        ans=False
+        break
+    else:
+        ans=True
+print(ans)
+
+#method-2 
+num  = input("enter the number: ")
+is_digit = False
+for i in num:
+    if i.isdigit():
+        is_digit = True
+    else:
+        is_digit = False
+        break
+print(is_digit)
