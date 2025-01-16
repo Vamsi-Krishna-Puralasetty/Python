80139ac8232c8fbccf2f36e9c64fecae24232832
Vamsi-Krishna-Puralasetty <puralasettyv@gmail.com>
2025-01-16 16:50:55 +0530

    sum of even num in array

--- Easy_level/Array9.py
+++ Easy_level/Array9.py
@@ -7,14 +7,12 @@
 # The even numbers in the array are 2 and 4. Their sum is 2  
 
 #method-1
-
 def sum_of_even(arr):
     res=[]
     for i in arr:
         if i%2==0:
             res.append(i)
     return sum(res)
-
 n = int(input("enter the number of elements: "))
 arr=[]
 for i in range(0,n):
