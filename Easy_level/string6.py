c8c34d23a07de67f9eac865180edefae7c7e6b26
Vamsi-Krishna-Puralasetty <puralasettyv@gmail.com>
2025-01-16 23:18:13 +0530

    convert str to int

---
+++ Easy_level/string6.py
@@ -0,0 +1,11 @@
+# Convert String to Number
+# Question: Write a function to convert a string to a number (without using parseInt or Number).
+# Testcase: "123"
+# Output: 123
+# Explanation: The string "123" is converted to the number 123.
+
+str=input("Enter the string : ")
+print(type(str))
+str=int(str)
+print(str)
+print(type(str))
